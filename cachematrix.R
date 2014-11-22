9240ad900599be143dadfe895f483a3947800d26
repin2000 <repin2000@gmail.com>
2014-11-22 11:12:05 +0300

    Programming Assignment 2.  Should work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Tried a little OOP here...
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invPr <- NULL
+    ## settting the matrix
+    set <- function( matrix ) {
+        m <<- matrix
+        invPr <<- NULL
+    }
+    
+    ## getting the matrix
+    get <- function() {
+        return(m)
+    }
+    
+    ## setting the inverse 
+    setInv <- function(inv) {
+        invPr <<- inv
+    }
+    
+    ## getting the inverse
+    getInv <- function() {
+        invPr
+    }
+    
+    ## Return a list of the methods
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInv()
+    
+    ## Some error handling
+    if( !is.null(m) ) {
+        return(m)
+    }
+    data <- x$get()
+    
+    ## calculate & return
+    m <- solve(data) %*% data
+    x$setInv(m)
+    return(m)
 }
